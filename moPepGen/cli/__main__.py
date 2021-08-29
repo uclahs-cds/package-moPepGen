40627bb6cbe0d54d98431335484953aae8c7ce71
zhuchcn <ChenghaoZhu@mednet.ucla.edu>
2021-08-28 17:59:21 -0700

    renaming callPeptide to callVariant, and redesigned the cli help page, to be more readable

--- moPepGen/cli/__main__.py
+++ moPepGen/cli/__main__.py
@@ -3,13 +3,42 @@ import sys
 import argparse
 from moPepGen import cli
 
+CLI_MAIN_USAGE = "moPopGen [-h] [-V] <command> [options]"
+CLI_MAIN_DESCRIPTION = """
+-- Indexing
+   generateIndex       Generate genome and proteome index files for moPepGen
+
+-- Parsing
+   parseVEP            Parse VEP output.
+   parseREDItools      Parse REDItools annotated output.
+   parseSTARFusion     Parse STAR-Fusion output.
+   parseFusionCatcher  Parse FusionCatcher output.
+   parseRMATS          Parse rMATS output.
+   parseCIRCexplorer   Parse CIRCexplorer known circRNA output.
+
+-- Calling
+   callVariant         Call non-canonical peptides from genomic variants.
+   callNoncoding       Call non-canonical peptides from noncoding transcripts.
+"""
 
 def main():
     """ Main entry point """
+
     parser = argparse.ArgumentParser(
-        prog='moPopGen'
+        prog='moPopGen',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        usage=CLI_MAIN_USAGE
     )
-    subparsers = parser.add_subparsers(dest='command')
+    parser.add_argument(
+        '-V', '--version',
+        help='Show version number and exit.',
+        action='store_true'
+    )
+    subparsers = parser.add_subparsers(
+        dest='command',
+        help=argparse.SUPPRESS,
+        description=CLI_MAIN_DESCRIPTION
+    )
 
     cli.add_subparser_generate_index(subparsers)
     cli.add_subparser_parse_vep(subparsers)
@@ -18,7 +47,7 @@ def main():
     cli.add_subparser_parse_fusion_catcher(subparsers)
     cli.add_subparser_parse_rmats(subparsers)
     cli.add_subparser_parse_circexplorer(subparsers)
-    cli.add_subparser_call_peptides(subparsers)
+    cli.add_subparser_call_variant(subparsers)
     cli.add_subparser_call_noncoding(subparsers)
 
     args = parser.parse_args()
@@ -26,6 +55,11 @@ def main():
     if len(sys.argv)==1:
         parser.print_help(sys.stderr)
         sys.exit(1)
+
+    if args.version:
+        from moPepGen import __version__
+        print(f'moPepGen {__version__}', file=sys.stdout, flush=True)
+        sys.exit()
 
     args.func(args)
 
