b2f8f1521026049bc323a94c74c54443af82285e
zhuchcn <ChenghaoZhu@mednet.ucla.edu>
2022-01-12 16:21:48 -0800

    fixed (PVG): subgraph end node was not defined properly

--- moPepGen/svgraph/PeptideVariantGraph.py
+++ moPepGen/svgraph/PeptideVariantGraph.py
@@ -85,7 +85,9 @@ class PeptideVariantGraph():
 
     def node_is_subgraph_end(self, node:PVGNode) -> bool:
         """ check if a node is the last node of a subgraph """
-        return node.subgraph_id != self.id and node.is_subgraph_end()
+        return all(x.subgraph_id == node.subgraph_id for x in node.in_nodes) and \
+            any(x.subgraph_id != node.subgraph_id and x is not self.stop for
+                x in node.out_nodes)
 
     def cleave_if_possible(self, node:PVGNode,
             return_first:bool=False) -> PVGNode:
