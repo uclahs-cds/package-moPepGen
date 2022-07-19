3f1850354cf19c0fea3bb93cee8c426c3e618971
zhuchcn <ChenghaoZhu@mednet.ucla.edu>
2022-07-18 22:52:50 -0700

    test (callVariant): test case added caught from fuzz test

--- test/integration/test_call_variant_peptides.py
+++ test/integration/test_call_variant_peptides.py
@@ -610,3 +610,12 @@ class TestCallVariantPeptides(TestCaseIntegration):
         expected = self.data_dir/'fuzz/08/brute_force.txt'
         reference = self.data_dir/'downsampled_reference/ENST00000314675.11'
         self.default_test_case(gvf, reference, expected)
+
+    def test_call_variant_peptide_case37(self):
+        """ Coding TX with in-frame deletion. #515 """
+        gvf = [
+            self.data_dir/'fuzz/09/fake_variants.gvf'
+        ]
+        expected = self.data_dir/'fuzz/09/brute_force.txt'
+        reference = self.data_dir/'downsampled_reference/ENST00000452737.5'
+        self.default_test_case(gvf, reference, expected)
