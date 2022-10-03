d394d9a3951b1ebf32e6c2f4491518164eb0010e
Chenghao Zhu <zhuchcn@gmail.com>
2022-10-03 16:46:33 +0800

    fix (ThreeFrameTVG): ensures that in-frame subgraph nodes are not treated as subgraph when aligning variant bubbles

--- test/integration/test_brute_force.py
+++ test/integration/test_brute_force.py
@@ -271,3 +271,12 @@ class TestBruteForce(TestCaseIntegration):
         expected = self.data_dir/'fuzz/25/brute_force.txt'
         reference = self.data_dir/'downsampled_reference/ENST00000265138.4-ENST00000650150.1'
         self.default_test_case(gvf, reference, expected)
+
+    def test_brute_force_fuzz_26(self):
+        """ Fuzz test 26 """
+        gvf = [
+            self.data_dir/'fuzz/26/fake_variants.gvf'
+        ]
+        expected = self.data_dir/'fuzz/26/brute_force.txt'
+        reference = self.data_dir/'downsampled_reference/ENST00000265138.4-ENST00000650150.1'
+        self.default_test_case(gvf, reference, expected)
